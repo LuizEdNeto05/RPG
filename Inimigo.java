aca168c13f09f01d48fdbbbbc92a1e8ae318f75c
Yuri Severo Pinto Costa <127903285+Yuri-Severo@users.noreply.github.com>
2023-09-24 12:12:36 -0300

    Adição da classe inimigos
    Classe para construção dos inimigos presentes no jogo

---
+++ Inimigo.java
@@ -0,0 +1,79 @@
+public class Inimigo {
+    public String inimigoNome;
+    public double inimigoVida;
+    public int inimigoForca;
+    public int inimigoDefesa;
+    public int inimigoAgilidade;
+
+    public Inimigo(String inimigoNome, double inimigoVida, int inimigoForca, int inimigoDefesa, int inimigoAgilidade){
+        setinimigoNome(inimigoNome);
+        this.inimigoVida = inimigoVida;
+        setinimigoForca(inimigoForca);
+        this.inimigoDefesa= inimigoDefesa;
+        this.inimigoAgilidade=inimigoAgilidade;
+    }
+
+
+    public void setinimigoNome(String inimigoNome)
+    {
+        if(inimigoNome.length()>0) {
+            this.inimigoNome = inimigoNome;
+        } 
+
+    }
+
+    public void setinimigoVida(int inimigoVida)
+    {
+        if(inimigoVida>0){
+            this.inimigoVida=inimigoVida;
+        }
+
+    }
+    public void setinimigoDefesa(int inimigoDefesa)
+    {
+        if(inimigoDefesa>0){
+            this.inimigoDefesa=inimigoDefesa;
+        }
+
+    }
+    public void setinimigoForca(int inimigoForca)
+    {
+        if(inimigoForca>0){
+            this.inimigoForca=inimigoForca;
+        }
+
+    }
+    public void setinimigoAgilidade(int inimigoAgilidade)
+    {
+        if(inimigoAgilidade>0){
+            this.inimigoAgilidade=inimigoAgilidade;
+        }
+
+    }
+
+
+    public String getinimigoNome(){
+        return inimigoNome;
+    }
+    public double getinimigoVida(){
+        return inimigoVida;
+    }
+    public int getinimigoForca(){
+        return inimigoForca;
+    }
+
+    public int getinimigoDefesa(){
+        return inimigoDefesa;
+    }
+    public int getinimigoAgilidade(){
+        return inimigoAgilidade;
+    }
+    public void InimigoAtributos(){
+        String nome = getinimigoNome();
+        double vida = getinimigoVida();
+        int forca = getinimigoForca();
+        int defesa = getinimigoDefesa();
+
+        
+    }
+}
