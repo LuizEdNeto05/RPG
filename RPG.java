4e1aa099069d466ffcc5578b8148556f9d884c37
Luiz Eduardo Neto <125611876+LuizEdNeto05@users.noreply.github.com>
2023-09-24 19:43:04 -0300

    Update RPG.java

--- RPG.java
+++ RPG.java
@@ -35,8 +35,11 @@ public class RPG {
         Scanner scanner2 = new Scanner(System.in);
         Scanner nome = new Scanner(System.in);
         String player;
+        int cura = 3, curainimigo =3;
+        Random rand = new Random();
         
         //List<Inimigo> inimigos1 = new ArrayList<>(); 
+        
          
         //Menu
         System.out.println("                                                            ");
@@ -56,7 +59,7 @@ public class RPG {
         //Input nome
         System.out.println("Olá herói! Me diga, qual é o seu nome?");
         player = nome.nextLine();
-        Jogador jogador = new Jogador(player,100, 0,0,0,0,new Arma(),new Armadura());
+        Jogador jogador = new Jogador(player,0, 0,0,0,0,new Arma(),new Armadura());
 
         limparTela();
 
@@ -76,35 +79,100 @@ public class RPG {
         limparTela();
         
         //->Definição do dano da arma
-        double danoJogador=0;
+        double danoJogadorLeve=0, danoJogadorPesado =0;
         if (escolhaArma==1) {
-            danoJogador = Arma.ArmaLeve(jogador.destreza);
-            System.out.println("Sua arma causa "+danoJogador+" de dano");
+            danoJogadorLeve = Arma.ArmaLeve(jogador.destreza);
+            System.out.println("Sua arma causa "+danoJogadorLeve+" de dano");
             
         }else if (escolhaArma==2) {
-            danoJogador = Arma.ArmaPesada(jogador.forca);
-            System.out.println("Sua arma causa "+danoJogador+" de dano");
+            danoJogadorPesado = Arma.ArmaPesada(jogador.forca);
+            System.out.println("Sua arma causa "+danoJogadorPesado+" de dano");
             
         }
 
         //Definição da defesa da Armadura
-        double defesaJogador= Armadura.ArmaduraSimples(jogador.constituicao);
+        double defesaJogador = Armadura.ArmaduraSimples(jogador.constituicao);
 
         //Exibição dos status do Jogador
         System.out.println("Status de "+jogador.getNome());
-        System.out.println("Vida: "+jogador.pv);
+        
         System.out.println("Defesa: "+defesaJogador);
-        System.out.println("Dano: " +danoJogador);
+        System.out.println("Dano: " +danoJogadorLeve);
         System.out.println("Agilidade: " +jogador.agilidade);
+        limparTela();
+
+        jogador.pv = jogador.getPv();
         
 
         //Combate 1
-        Inimigo Goblin = new Inimigo("Goblin",100.00, 30, 45, 5);
-        do{
-            if(jogador.agilidade>inimigoAgilidade){
-                
+        Inimigo Goblin = new Inimigo("Goblin",100, 30, 5, 5);
+
+        Scanner acao = new Scanner(System.in);
+        int agir;
+        while (true){
+            if(jogador.agilidade>Goblin.inimigoAgilidade){
+                System.out.println("Goblin: "+ Goblin.inimigoVida);
+                System.out.println(""+jogador.getNome() +jogador.pv);
+                agir = acao.nextInt();
+
+                if(agir == 1)
+                {
+                    double danototal = danoJogadorLeve - Goblin.inimigoDefesa;
+                    Goblin.inimigoVida -= danototal;
+
+                }else if(agir == 2)
+                {
+                    double defesatotal = defesaJogador*2;
+                    System.out.println("A defesa aumentou");
+                }else if(agir == 3)
+                {
+                    
+                    if(cura>0)
+                    {
+                        
+                    int curaTotal = dado6() + dado6() + dado6();
+                    jogador.pv += curaTotal;
+                    cura --;
+                    }else if(cura <= 0){
+                        System.out.println("voce n tem mais cura");
+
+                    }
+                    
+
+                }
+                int escolhaInimigo = rand.nextInt(3) + 1;
+
+                if(escolhaInimigo == 1)
+                {
+                    int danoNpc = dado6() + dado6() + dado6() + 5;
+                    double danoNPC = danoNpc - defesaJogador;
+                    jogador.pv -= danoNPC;
+                }else if(escolhaInimigo == 2)
+                {
+                    int def = Goblin.inimigoDefesa * 2;
+                    System.out.println("Defesa aumentada para: "+def);
+                }else if(escolhaInimigo == 3)
+                {
+                    if(curainimigo>0)
+                    {
+                        
+                    int curaEnemy = dado6() + dado6() + dado6();
+                    jogador.pv += curaEnemy;
+                    curainimigo --;
+                    }else if(cura <= 0){
+                        System.out.println("adversario tenta curar mas ele n tem mais cura");
+
+                    }
+                }
+
+
+                if(Goblin.inimigoVida <= 0 || jogador.pv<=0)
+                {
+                    break;
+                }
+
             }
-        }while(inimigoVida>0 || jogador.pv>0);
+        }
 
         /*}else if(x == 2){      
             System.out.println("Lore"); //LORE A SER DEFINIDA
@@ -118,6 +186,24 @@ public class RPG {
 
         }
 }
+
+/*public void acoesPersonagem()
+    {
+        int acao;
+        Scanner agir = new Scanner(System.in);
+        int cura = 3;
+
+
+        System.out.println("É a sua vez, o que deseja fazer?\n");
+        System.out.println("1) Atacar\n 2) Defender\n 3) Curar\n");
+
+        acao = agir.nextInt();
+
+        if(acao == 1)
+        {
+            Goblin.inimigoVida -= ArmaLeve(); ;
+        }
+    } */
 }
     
      /*public static Inimigo sortearInimigoAleatorio(List<Inimigo> inimigos) {
