7926a09cbd1cd82f3a2495358d848de902f5e96b
Yuri Severo Pinto Costa <127903285+Yuri-Severo@users.noreply.github.com>
2023-09-24 02:18:27 -0300

    Update Arma
    Adição das funções dos dados para números aleatórios, adição dos dois tipos de armas, finalização dos encapsulamentos

--- Arma.java
+++ Arma.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Arma {
     String categoria;
     int danoConstante;
@@ -12,5 +14,57 @@ public class Arma {
     {
         this.categoria = categoria;
         this.danoConstante = danoConstante;
+    }
+
+    public void setCategoria(String categoria)
+    {
+        if(categoria.length()>0) {
+            this.categoria = categoria;
+        } 
+    }
+
+    public String getCategoria()
+    {
+        return categoria;
+    }
+
+    public void setdanoConstante(int danoConstante)
+    {
+        if(danoConstante>0)
+        {
+            this.danoConstante=danoConstante;
+        }
+    }
+
+    public int getdanoConstante(){
+        return danoConstante;
+    }
+
+    // Função para sortear um número entre 1 e 4
+    public static int dado4() {
+        Random random = new Random();
+        return random.nextInt(4) + 1;
+    }
+
+    // Função para sortear um número entre 1 e 6
+    public static int dado6() {
+        Random random = new Random();
+        return random.nextInt(6) + 1;
+    }
+
+    // Função para sortear um número entre 1 e 12
+    public static int dado12() {
+        Random random = new Random();
+        return random.nextInt(12) + 1;
+    }
+
+    public static double ArmaLeve(int destreza){
+        double danoArmaLeve = 15 + dado4() + dado4() + dado4() + destreza;
+        return danoArmaLeve;
+    }
+
+    public static double ArmaPesada(int forca){
+        double danoArmaPesada = 30 + dado12() + (1.5*forca);
+        return danoArmaPesada;
     }
 }
