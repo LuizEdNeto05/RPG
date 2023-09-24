66d9d2ee74d5cae89e440259b17cac5d91fd7bc2
Yuri Severo Pinto Costa <127903285+Yuri-Severo@users.noreply.github.com>
2023-09-24 02:19:38 -0300

    Update Armadura
    Finalização dos encapsulamentos, adição da armadura simples

--- Armadura.java
+++ Armadura.java
@@ -11,4 +11,32 @@ public class Armadura {
     {
         this.defesa = defesa;
     }
+
+    public void setdefesa(int defesa)
+    {
+        if(defesa>0)
+        {
+            this.defesa=defesa;
+        }
+    }
+
+    public int getdanoConstante()
+    {
+        return defesa;
+    }
+
+    public static double ArmaduraSimples(int constituicao){
+        double ArmaduraLeve = 20 + (1.5*constituicao);
+        return ArmaduraLeve;
+    }
+
+    /*public static double ArmaduraMedia(int constituicao){
+        double ArmaduraMedia = 30 + (1.5*constituicao);
+        return ArmaduraMedia;
+    }
+
+    public static double ArmaduraPesada(int constituicao){
+        double ArmaduraPesada = 40 + (1.5*constituicao);
+        return ArmaduraPesada;
+    }*/
 }
