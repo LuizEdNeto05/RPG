ce1420b8c574423ec788de4a3ac4f72269e40ae7
Luiz Eduardo Neto <125611876+LuizEdNeto05@users.noreply.github.com>
2023-09-26 18:17:21 -0300

    Create Pocao.java

---
+++ Pocao.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+public class Pocao {
+    private int pontos;
+
+
+    public Pocao(int pontos)
+    {
+        setPontos(pontos);
+    }
+
+    public void setPontos(int pontos) {
+        if(pontos>0){
+            this.pontos = pontos;
+        }
+
+        
+    }
+
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public static int dado6() {
+        Random random = new Random();
+        return random.nextInt(6) + 1;
+    }
+
+    public static int usarPocao( int pontos) {
+        
+        
+            pontos = dado6() + dado6() + dado6();
+
+            
+            System.out.println("Você usou uma poção de cura e recuperou " + pontos + " pontos de vida.");
+            return pontos;
+           
+
+    
+}
+}
